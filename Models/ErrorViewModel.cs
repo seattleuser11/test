f63bf641635cdf4107f316927cfbb085013ad2a4
seattleuser11 <seattleuser11@gmail.com>
2019-02-13 13:23:15 -0800

    new app

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace basicapp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
